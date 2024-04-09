d5dee01c92cb95e187366acad855712e83d406ec
nichathong <nicha.fn@gmail.com>
2024-04-09 13:59:23 -0700

    Completed first question

--- 1/problem.py
+++ 1/problem.py
@@ -3,7 +3,28 @@ class ListNode:
         self.val = val
         self.next = next
 
+# question ask to remove duplicate
+# since it is sorted -> the number will appear in order
+# we can start by traverse through linked link
+    #check if current.val == current.next.val?
+    #if it's equal then we want to remove
+        #to remove
+            # current.next= current.next.next
+            #
+            #else current = current.next
 class Solution:
     def deleteDuplicates(self, head):
-        # Remove pass and write code here
-        pass
+        if not head:
+            return None
+        
+        current = head
+
+        while current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return head
+        
+        
