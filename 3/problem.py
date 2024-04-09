583885efb0eb76dfdc6fa8697ba29a7524f8d4aa
nichathong <nicha.fn@gmail.com>
2024-04-09 14:49:51 -0700

    Added the last answer

--- 3/problem.py
+++ 3/problem.py
@@ -1,4 +1,18 @@
 class Solution:
+    #base case -> check if the len are the same
     def letterCasePermutation(self, s):
-        # Remove pass and write code here
-        pass
+
+        def _backtrack(idx, current_permutation):
+            if idx == len(s):
+                result.append(current_permutation)
+                return 
+            #need to check if character is alphabet?
+            if s[idx].isalpha():
+                _backtrack(idx+1, current_permutation + s[idx].upper())
+            else:
+                _backtrack(idx+1, current_permutation + s[idx])
+
+        result = []
+        _backtrack(0, "")
+        return result
+
